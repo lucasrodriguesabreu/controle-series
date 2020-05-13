e9a991aab247207af7899c07123b00154c01c70d
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-05-13 04:56:54 -0300

    Página de teste inserida em /series

---
+++ resources/views/series/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+    content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Controle de Séries</title>
+</head>
+<body>
+    Olá mundo, Séries!
+</body>
+</html>
