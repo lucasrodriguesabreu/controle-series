8b6be451363d935b30ec7b6c3fb627551f2ca985
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-05-20 05:02:11 -0300

    Visualização de episódio implementado

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,10 @@ Route::delete('/series/{id}', 'SeriesController@destroy');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
+
+Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
