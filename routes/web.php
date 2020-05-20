f54f0cd448327b684ca1f4c14e833b1339c8e284
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-05-20 07:15:38 -0300

    Login estilizado implementado

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,21 @@
 Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')
-    ->name('form_criar_serie');
-Route::post('/series/criar', 'SeriesController@store');
-Route::delete('/series/{id}', 'SeriesController@destroy');
-Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+    ->name('form_criar_serie')
+    ->middleware('autenticador');
+Route::post('/series/criar', 'SeriesController@store')
+    ->middleware('autenticador');
+Route::delete('/series/{id}', 'SeriesController@destroy')
+    ->middleware('autenticador');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
+    ->middleware('autenticador');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
 
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
+    ->middleware('autenticador');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -31,3 +36,8 @@ Route::get('/entrar', 'EntrarController@index');
 Route::post('/entrar', 'EntrarController@entrar');
 Route::get('/registrar', 'RegistroController@create');
 Route::post('/registrar', 'RegistroController@store');
+
+Route::get('/sair', function () {
+    \Illuminate\Support\Facades\Auth::logout();
+    return redirect('/entrar');
+});
